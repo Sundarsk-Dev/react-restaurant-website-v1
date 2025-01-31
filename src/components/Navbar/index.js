7b4b6605c93e55e2c3cf6fcf36c9203263a1954a
Sundarsk-Dev <kamalsundarsk@gmail.com>
2025-01-31 11:39:24 +0530

    Initial commit

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { Nav, NavLink, NavIcon, Bars } from './NavbarElements';
+import { Link } from 'react-router-dom';
+import { Menu } from 'lucide-react';
 
 const Navbar = ({ toggle }) => {
   return (
-    <>
-      <Nav>
-        <NavLink to='/'>Pizza</NavLink>
-        <NavIcon onClick={toggle}>
-          <p>Menu</p>
-          <Bars />
-        </NavIcon>
-      </Nav>
-    </>
+    <nav className="bg-gradient-to-r from-red-500 to-yellow-500 shadow-lg p-4 flex justify-between items-center">
+      {/* Logo */}
+      <Link to="/" className="text-white text-2xl font-bold flex items-center gap-2">
+        🍕 <span>PizzaHub</span>
+      </Link>
+      
+      {/* Menu Icon */}
+      <button onClick={toggle} className="text-white text-lg flex items-center gap-2 px-4 py-2 bg-black rounded-lg hover:bg-opacity-80 transition-all">
+        <span>Menu</span>
+        <Menu size={24} />
+      </button>
+    </nav>
   );
 };
 
